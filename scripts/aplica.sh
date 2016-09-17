6ebb802938b3e54db1fabf9bc3402153941427a4
root <root@nvtapps>
2016-09-17 00:56:57 -0300

    Criacao do script de sicronizacao

--- scripts/aplica.sh
+++ scripts/aplica.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Objetivo: Script para sicnronizar as alterações realizadas e homologadas no produto
+# Proprietario: Felipe Figueiredo <ffigueiredo@nvt.com.br>
+# Historico: 
+# Criacao: 21/09
 
+DSTSERVER=nvtpbx
+
+echo $DSTSERVER

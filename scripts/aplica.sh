7ef14d6a47770acbab91ecbdd486fdba61faf2ed
root <root@nvtapps>
2016-12-01 00:59:32 -0200

    Alteracao do script aplica.sh - Adicionando novo servidor asterisk

--- scripts/aplica.sh
+++ scripts/aplica.sh
@@ -4,9 +4,15 @@
 # Proprietario: Felipe Figueiredo <ffigueiredo@nvt.com.br>
 # Historico: 
 # Criacao: 21/09
+# Alteração: 05/12 - Adição do segundo servidor pabx para sincronização
 
-DSTSERVER=nvtpabx
+
+for DSTSERVER in nvtpabx nvtpabx nvtpabx2 ; 
+
+do
 
 echo $DSTSERVER
 
 sshpass -p 'suportenvt' rsync -avzh /home/suportenvt/produtos/smartura/asterisk/etc/asterisk/ -e 'ssh -p 22' suportenvt@$DSTSERVER:/home/suportenvt/asterisk
+
+done
